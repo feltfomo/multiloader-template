00c46a34ea578702d3ab75f46da9e899fb349cc4
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-08-19 05:53:37 -0700

    ci: align nushell and template formatting

--- template/formatter.nix
+++ template/formatter.nix
@@ -10,7 +10,16 @@
     "**/run/**"
   ];
 
-  programs.google-java-format.enable = true;
+  programs.google-java-format = {
+    enable = true;
+    # loader shims contain scaffold tokens until a project is generated
+    excludes = [
+      "fabric/src/main/java/**/FabricEntry.java"
+      "fabric/src/client/java/**/FabricClientEntry.java"
+      "neoforge/src/main/java/**/NeoForgeEntry.java"
+      "neoforge/src/main/java/**/NeoForgeClientEntry.java"
+    ];
+  };
   programs.ktfmt.enable = true;
   programs.nixfmt.enable = true;
   programs.prettier.enable = true;
